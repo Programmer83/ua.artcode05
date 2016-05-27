d8a384445d86d415b74f76522bdc69161e9adbda
Programmer83 <dimatolm@gmail.com>
2016-05-28 01:43:13 +0300

    new

---
+++ .idea/src/ua/artcode05/week1/New.java
@@ -0,0 +1,16 @@
+package ua.artcode05.week1;
+
+import java.util.Scanner;
+
+/**
+ * Created by Programmer on 28/05/2016.
+ */
+public class New {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please inpute ");
+
+
+    }
+}
