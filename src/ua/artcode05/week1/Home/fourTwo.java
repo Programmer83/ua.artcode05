29e4c71728a044952c6692a8937f7e83a34ae279
Programmer83 <dimatolm@gmail.com>
2016-06-04 01:17:42 +0300

    week2

--- src/ua/artcode05/week1/Home/fourTwo.java
+++ src/ua/artcode05/week1/Home/fourTwo.java
@@ -2,9 +2,6 @@ package ua.artcode05.week1.Home;
 
 import java.util.Scanner;
 
-/**
- * Created by Programmer on 28/05/2016.
- */
 public class fourTwo {
     public static void main(String[] args) {
 
